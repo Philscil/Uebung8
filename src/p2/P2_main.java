62882a610b5686ae19038b6a89243dc26f70462e
Philscil <Philipp.Linder@web.de>
2025-12-11 13:38:00 +0100

    P2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,13 @@
+package p2;
+
+public class P2_main {
+    public static void main(String[] args) {
+        System.out.println(shortenToLowerCase("RindfLeischetikETTierung", 13));
+    }
+
+    public static String shortenToLowerCase(String input, int shortenBy) {
+        input  = input.substring(0 , input.length() - shortenBy);
+        input = input.toLowerCase();
+        return input;
+    }
+}
