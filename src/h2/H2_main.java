11481d09cb99f80bb9e4e83fb3717a55dafd3bd7
Philscil <Philipp.Linder@web.de>
2025-12-11 14:35:54 +0100

    H2

---
+++ src/h2/H2_main.java
@@ -0,0 +1,10 @@
+package h2;
+
+public class H2_main {
+    public static void main(String[] args) {
+        Simplelist list = new Simplelist();
+        list.append(10);
+        list.append(20);
+        list.append(30);
+    }
+}
---
+++ src/h2/Simplelist.java
@@ -0,0 +1,86 @@
+package h2;
+
+public class Simplelist {
+    Node head;
+
+    public Simplelist() {
+        Node n = new Node(Integer.MIN_VALUE);
+        head = n;
+    }
+
+    public Node getfirst() {
+        if (head.next == null) {
+            return null;
+        }
+
+        Node n = head.next;
+        return n;
+    }
+
+    public Node getLast() {
+        Node current = head;
+
+        if (current.next == null) {
+            return null;
+        }
+
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        return current;
+    }
+
+    public void append(int newValue) {
+        Node n = getLast();
+
+        Node newNode = new Node(newValue);
+
+        if (n == null) {
+            head.next = newNode;
+        } else {
+            n.next = newNode;
+        }
+    }
+    
+    public Node findFirst (int value) {
+        Node current = head.next;
+
+        while (current != null) {
+            if (current.value == value) {
+                return current;
+            }
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    public boolean insertAfter (int preValue, int newValue) {
+        Node preNode = findFirst(preValue);
+
+        if (preNode == null) {
+            return false;
+        }
+
+        Node newNode = new Node(newValue);
+        newNode.next = preNode.next;
+        preNode.next = newNode;
+
+        return true;
+    }
+
+    public boolean delete (int value) {
+        Node current = head;
+
+        while (current.next != null) {
+            if (current.next.value == value) {
+                current.next = current.next.next;
+                return true;
+            }
+            current = current.next;
+        }
+
+        return false;
+    }
+}
